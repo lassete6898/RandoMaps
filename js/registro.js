34de77ee80ad7ebe81d9fc800e5ce931f9b73f2b
Juan Lasso de la Vega <juan@MacBook-Pro-de-Juan.local>
2020-12-03 16:33:06 +0100

    cambios

--- js/registro.js
+++ js/registro.js
@@ -12,7 +12,6 @@ firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
 
 var db = firebase.firestore();
-// var nuevoID;
 
 function registro() {
   var nombre = document.getElementById("nombre").value;
