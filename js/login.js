5c0c8b7b4defa8f1180bff09d96d1559d0b38882
Juan Lasso de la Vega <juan@MacBook-Pro-de-Juan.local>
2020-12-06 14:01:09 +0100

    arreglo

--- js/login.js
+++ js/login.js
@@ -16,7 +16,10 @@ function login() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(function (error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
